a44a0ebf205409227c0bed45eb0f85f13435df48
nathan <nathanpatrick939@gmail.com>
2021-07-22 13:33:18 -0400

    boiler plate set up of Mongoose express backend

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+
+require('dotenv').config();
+
+//create server
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('mongoDB database connection established succeccfully');
+});
+
+app.listen(port, () => {
+    console.log(`server is running on port: ${port}`);
+});
