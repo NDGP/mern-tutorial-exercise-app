257389be79aa6e58019955818c436366e1bc008c
nathan <nathanpatrick939@gmail.com>
2021-07-22 16:29:34 -0400

    components built out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import ExercisesList from './Components/ExercisesList';
+import EditExercise from './Components/EditExercise';
+import CreateExercise from './Components/CreateExercise';
+import CreateUser from './Components/CreateUser';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
-    <div className="container">
-      Hello World!
-    </div>
+    <Router>
+      <NavBar />
+      <br />
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/" component={CreateExercise} />
+      <Route path="/" component={CreateUser} />
+    </Router>
   );
 }
 
